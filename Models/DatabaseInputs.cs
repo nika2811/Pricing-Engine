db3324e10169d4b0ac4b972254b0a1fe6856c885
Nika <nika.nacvlishvili1@gmail.com>
2023-02-13 20:52:34 +0400

    DatabaseInput

---
+++ Models/DatabaseInputs.cs
@@ -0,0 +1,10 @@
+﻿namespace Pricing_Engine.Models
+{
+    public class DatabaseInputs
+    {
+        public decimal MaintenanceRate { get; set; }
+        public decimal PrepaymentRate { get; set; }
+        public string CreditRiskAllocation { get; set; }
+        public decimal CapitalRiskRateWeight { get; set }
+    }
+}
--- Models/LoanAmortizationSchedule.cs
+++ Models/LoanAmortizationSchedule.cs
@@ -8,9 +8,9 @@
         public decimal ContractualPrincipal { get; set; }
         public decimal BaloonPaymentAtMaturity { get; set; }
         public decimal TotalContractualCashflow { get; set; }
-        public decimal PrepaymentCashflow { get; set; }
+        public decimal PrepaymentCashFlow { get; set; }
         public decimal TotalPrincipalPaid { get; set; }
-        public decimal AnnaulizedInterestOnCashflow { get; set; }
+        public decimal AnnaulizedInterestOnCashFlow { get; set; }
         public decimal BeginningBalance { get; set; }
         public decimal EndingBalance { get; set; }
     }
